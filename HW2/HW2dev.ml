3b569368e51080a477e60686ca0ca45a33156b35
cbierman <cale-bierman@uiowa.edu>
2014-10-16 20:29:00 -0500

    (hopefully) completed 3.4

--- HW2/HW2dev.ml
+++ HW2/HW2dev.ml
@@ -1,63 +1,63 @@
 let int_of_char ch = 
-	match ch with
-	| "a" -> 2
-	| "b" -> 2
-	| "c" -> 2
+  match ch with
+  | "a" -> 2
+  | "b" -> 2
+  | "c" -> 2
 
-	| "d" -> 3
-	| "e" -> 3
-	| "f" -> 3
+  | "d" -> 3
+  | "e" -> 3
+  | "f" -> 3
 
-	| "g" -> 4
-	| "h" -> 4
-	| "i" -> 4
+  | "g" -> 4
+  | "h" -> 4
+  | "i" -> 4
 
-	| "j" -> 5
-	| "k" -> 5
-	| "l" -> 5
+  | "j" -> 5
+  | "k" -> 5
+  | "l" -> 5
 
-	| "m" -> 6
-	| "n" -> 6
-	| "o" -> 6
+  | "m" -> 6
+  | "n" -> 6
+  | "o" -> 6
 
-	| "p" -> 7
-	| "q" -> 7
-	| "r" -> 7
-	| "s" -> 7
+  | "p" -> 7
+  | "q" -> 7
+  | "r" -> 7
+  | "s" -> 7
 
-	| "t" -> 8
-	| "u" -> 8
-	| "v" -> 8
+  | "t" -> 8
+  | "u" -> 8
+  | "v" -> 8
 
-	| "w" -> 9
-	| "x" -> 9
-	| "y" -> 9
-	| "z" -> 9
+  | "w" -> 9
+  | "x" -> 9
+  | "y" -> 9
+  | "z" -> 9
 
-	| _ -> raise Not_found
+  | _ -> raise Not_found
 ;;
 
 let rec intlist_of_string string = 
-	match string with
-	| [] -> []
-	| h :: tl -> (int_of_char h) :: intlist_of_string tl
+  match string with
+  | [] -> []
+  | h :: tl -> (int_of_char h) :: intlist_of_string tl
 ;;
 
 let rec assoc key dict = 
-	match dict with
-	| [] -> raise Not_found
-	| (a,b) :: tl -> if a = key then b else assoc key tl
+  match dict with
+  | [] -> raise Not_found
+  | (a,b) :: tl -> if a = key then b else assoc key tl
 ;;
 
 
 let rec change key value dict = 
-	match dict with
-	| [] -> (key, value) :: dict
-	| (a,b) :: tl -> if a = key then (*change value found*) (a, value) :: dict 
-					else (*continue search*) (a,b) :: change key value tl
+  match dict with
+  | [] -> (key, value) :: dict
+  | (a,b) :: tl -> if a = key then (*change value found*) (a, value) :: dict 
+          else (*continue search*) (a,b) :: change key value tl
 ;;
 
-type ('a, 'b) trie = Node of 'b list * ('a * ('a, 'b) trie);;
+type ('a, 'b) trie = Node of 'b list * ('a * ('a, 'b) trie) list;;
 
 let words trie = match trie with
 | Node (words, _) -> words
@@ -67,8 +67,34 @@ let branches trie = match trie with
 | Node (_, branches) -> branches
 ;;
 
-let empty = Node ([], [(None, Node (None, []))]);;
+let empty = Node([], []);;
 
+let trie_of_key trie key =
+  match trie with
+  | Node(a, b) -> try assoc key b with Not_found -> empty
+  
+let rec find trie keys =
+  match keys with
+    | [] -> words trie
+    | h::tl -> find (trie_of_key trie h) tl
+  
+let add_word word trie =
+  match trie with
+    | Node(a,b) -> Node((word :: a), b)
+  
+;;
+  
+let replace key branch trie =
+  match trie with
+    | Node(a,b) -> Node(a, (change key branch b))
+;;
+  
+
+  
+  
+
+  
+  
 
 
 
